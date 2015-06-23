af732113ff4509f39007601498ca88fa109b5da2
zrKing <love8703@gmail.com>
2015-06-23 16:46:10 +0800

    first V1.0
    V1.0

---
+++ GetSSParam/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GetSSParam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(223, 227);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "  ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "   ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.DoubleClick += new System.EventHandler(this.label2_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(248, 276);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shadow参数获取";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GetSSParam/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace GetSSParam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Form1.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private string strURL = @"http://doc.wandoer.com/fp.txt";
+        private string str = "";
+        private string strVersion = "V1.0";
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+           
+            try
+            {
+                this.textBox1.TextAlign = HorizontalAlignment.Left;
+                this.textBox1.Text = "Please Wait";
+                this.button1.Enabled = false;
+                Thread t2 = new Thread(new ThreadStart(ShowLabel));
+                Thread t1 = new Thread(new ThreadStart(ThreadMethod));
+                t1.Start();
+                t2.Start();
+                t1.Join();
+                t2.Abort();
+                this.label1.Text = " ";
+            }
+            catch (System.Exception ex)
+            {
+                str = ex.ToString();
+                str += "\r\n";
+                str += "\r\n";
+                str += "\r\n";
+                str += "请稍后重试或联系@zrKing";
+            }
+            finally
+            {
+                this.button1.Enabled = true;
+            }
+
+            this.textBox1.Text = str;
+        }
+
+        private void ThreadMethod()
+        {
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            str = wc.DownloadString(strURL);
+            str = str.Replace("\n", "\r\n");
+            
+            this.textBox1.Text = str;
+        }
+
+        private void ShowLabel()
+        {
+            string strText = ".";
+            while (true)
+            {
+                strText += ".";
+                if (strText.Length >= 5)
+                    strText = ".";
+                this.label1.Text = strText;
+                Application.DoEvents();
+                Thread.Sleep(500);
+            }
+        }
+
+        private void label2_DoubleClick(object sender, EventArgs e)
+        {
+            string strAbout = "Get the params of Shadowsocks for wangfl.\r\nIf there any problems ,please contact zrKing©2015";
+            strAbout += "\r\n\r\nVersion:";
+            strAbout += strVersion;
+
+            this.textBox1.TextAlign = HorizontalAlignment.Center;
+            this.textBox1.Text = strAbout;
+        }
+    }
+}
